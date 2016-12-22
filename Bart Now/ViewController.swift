8df822d6764802be222d01c6347ddd49c1cd418c
dustin-chan-89 <busterdust@gmail.com>
2016-12-21 16:57:02 -0800

    Initial Commit

---
+++ Bart Now/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bart Now
+//
+//  Created by Dustin Chan on 12/21/16.
+//  Copyright © 2016 Dustin Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
